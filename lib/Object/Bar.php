af1af870c3db5b15b70ae5b7caf4eac0d140acd6
Don Bauer <lordgnu@me.com>
2012-12-30 23:50:15 -0600

    Added missing 2nd parameter

--- lib/Object/Bar.php
+++ lib/Object/Bar.php
@@ -43,7 +43,7 @@ class Bar extends AbstractObject
             }
         }
 
-        array_unshift($this->position);
+        array_unshift($this->position, $this->tail);
 
         if ($this->direction > 0) {
             $this->position = $this->nextPixel($this->position);
