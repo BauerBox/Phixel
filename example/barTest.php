12175c25526c3fed56fd9aa37033e47ece0c65ac
Don Bauer <lordgnu@me.com>
2013-01-01 00:54:54 -0600

    Removed debugging information

--- example/barTest.php
+++ example/barTest.php
@@ -9,7 +9,6 @@ include_once __DIR__ . '/../lib/Phixel.php';
 
 Phixel::installAutloader();
 Phixel::setMaxBrightness(0.5);
-Phixel::enableDebugOutput();
 
 $phixel = new Phixel(new LPD6803Driver('/dev/spidev0.0', 156));
 $phixel->allOff();
