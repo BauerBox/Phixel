60eb7a300f8f3b3402e26838b73ff2862172a41f
Don Bauer <lordgnu@me.com>
2013-01-01 00:47:08 -0600

    Updated Bar Test Script

--- example/barTest.php
+++ example/barTest.php
@@ -16,5 +16,8 @@ $phixel->allOff();
 
 $frame = new FrameBuffer($phixel);
 $frame->attachObject(new Bar(0xff0000, 5, 0, 1));
+$frame->attachObject(new Bar(0x00ff00, 5, 38, 1));
+$frame->attachObject(new Bar(0x0000ff, 5, 76, 1));
+$frame->attachObject(new Bar(0xffffff, 10, 95, -1));
 
 $frame->startLoop();
