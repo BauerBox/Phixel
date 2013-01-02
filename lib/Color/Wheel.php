d1e35ef692b51d182562f2a27a7a5b7ae1bece85
Don Bauer <lordgnu@me.com>
2013-01-02 17:04:55 -0600

    Added Wheel and Rainbow color utilities

---
+++ lib/Color/Wheel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BauerBox\Phixel\Color;
+
+use BauerBox\Phixel\Pixel\Pixel;
+
+class Wheel
+{
+    protected $pixel;
+
+    public function __construct()
+    {
+        $this->pixel = new Pixel;
+    }
+
+    public function __invoke($position)
+    {
+        if ($position < 85) {
+            return $this->pixel->setColorRGB($position * 3, 255 - $position, 0)->getColor();
+		} elseif ($position < 170) {
+			$position -= 85;
+            return $this->pixel->setColorRGB(255 - ($position * 3), 0, $position * 3)->getColor();
+		} else {
+			$position -= 170;
+            return $this->pixel->setColorRGB(0, $position * 3, 255 - $position * 3)->getColor();
+		}
+    }
+
+    public function next($position)
+    {
+        if ($position == 255) {
+            return 0;
+        }
+
+        return $position + 1;
+    }
+}
