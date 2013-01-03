345c71283e7a56d950cc8300ad7e60e2cfe98fe8
Don Bauer <lordgnu@me.com>
2013-01-02 21:27:56 -0600

    Adding more debugging

--- example/geoSphereTest.php
+++ example/geoSphereTest.php
@@ -5,8 +5,11 @@ use BauerBox\Phixel\Driver\LPD6803\LPD6803Driver;
 use BauerBox\Phixel\Object\GeodesicSphere;
 use BauerBox\Phixel\Buffer\FrameBuffer;
 use BauerBox\Phixel\Pixel\Pixel;
+use BauerBox\Phixel\Debug\Debug;
 
 include 'include/bootstrap.php';
+
+Phixel::enableDebugOutput();
 
 $phixel = new Phixel(new LPD6803Driver('/dev/spidev0.0', 156));
 
@@ -20,6 +23,12 @@ foreach (array(0x0000ff, 0x000ff0, 0x00ff00, 0x0ff000, 0xff0000, 0xf0000f) as $c
         $colors[] = $pixel->setColor($color)->setBrightness($i)->getCompiledColor();
     }
 }
+
+foreach ($colors as $color) {
+    Debug::logBinary($color);
+}
+
+exit(0);
 
 
 $sphere = new GeodesicSphere('Resources:Config:GeodesicSphereZones', $colors);
