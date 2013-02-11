c16889ddca14fc7d751dea406ff7f4e4c6d4d4b4
Don Bauer <lordgnu@me.com>
2013-02-11 13:02:07 -0600

    Testing WS2801 Driver

--- example/spi.barTest.php
+++ example/spi.barTest.php
@@ -1,23 +1,18 @@
 <?php
 
 use BauerBox\Phixel\Phixel;
-use BauerBox\Phixel\Driver\SPI\LPD6803\LPD6803Driver;
+use BauerBox\Phixel\Driver\WS2801Driver as Driver;
 use BauerBox\Phixel\Buffer\FrameBuffer;
 use BauerBox\Phixel\Object\Bar;
-use BauerBox\Phixel\Object\Pentagon;
 
 include_once __DIR__ . '/../lib/Phixel.php';
 
 Phixel::installAutloader();
-Phixel::setMaxBrightness(0.5);
 
-$phixel = new Phixel(new LPD6803Driver(0, 2000000, 192));
+$phixel = new Phixel(new Driver(25, Driver::MODE_SPI, 0));
 $phixel->allOff();
 
 $frame = new FrameBuffer($phixel);
 $frame->attachObject(new Bar(0xff0000, 5, 0, 1));
-$frame->attachObject(new Bar(0x00ff00, 5, 38, 1));
-$frame->attachObject(new Bar(0x0000ff, 5, 76, 1));
-$frame->attachObject(new Bar(0xffffff, 10, 95, -1));
 
 $frame->startLoop();
