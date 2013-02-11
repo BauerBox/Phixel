e7f63b24b20a6ecbc258e4dea3f464ca1eac29e6
Don Bauer <lordgnu@me.com>
2013-02-11 13:55:25 -0600

    Added WS fill test

---
+++ example/spi.ws.fillTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use BauerBox\Phixel\Phixel;
+use BauerBox\Phixel\Driver\WS2801Driver as Driver;
+use BauerBox\Phixel\Buffer\FrameBuffer;
+use BauerBox\Phixel\Object\Bar;
+
+include_once __DIR__ . '/../lib/Phixel.php';
+
+Phixel::installAutloader();
+Phixel::enableDebugOutput();
+
+$phixel = new Phixel(new Driver(25, Driver::MODE_SPI, 0));
+$phixel->allOff();
+
+for ($i = 0x000000; $i < 0xffffff; $i += 8) {
+    $phixel->fill($i);
+}
