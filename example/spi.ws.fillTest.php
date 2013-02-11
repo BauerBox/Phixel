b2c9896d035b8e9fd766b5b8a8dbf58c86171498
Don Bauer <lordgnu@me.com>
2013-02-11 13:56:34 -0600

    Updated fill test

--- example/spi.ws.fillTest.php
+++ example/spi.ws.fillTest.php
@@ -13,6 +13,6 @@ Phixel::enableDebugOutput();
 $phixel = new Phixel(new Driver(25, Driver::MODE_SPI, 0));
 $phixel->allOff();
 
-for ($i = 0x000000; $i < 0xffffff; $i += 8) {
+for ($i = 0x000000; $i < 0xffffff; $i += 0x0000f) {
     $phixel->fill($i);
 }
