ac115b9b1541bbe0867c878e6fda025352f8f943
Don Bauer <lordgnu@me.com>
2013-02-11 11:32:15 -0600

    Updated to test new LPD6803 driver

--- example/spi.fontTest.php
+++ example/spi.fontTest.php
@@ -1,18 +1,20 @@
 <?php
 
 use BauerBox\Phixel\Phixel;
-use BauerBox\Phixel\Driver\SPI\LPD6803\LPD6803Driver;
+use BauerBox\Phixel\Driver\LPD6803Driver;
 use BauerBox\Phixel\Buffer\FrameBuffer;
 use BauerBox\Phixel\Object\Font8x5;
 
 include_once __DIR__ . '/../lib/Phixel.php';
 
 Phixel::installAutloader();
-Phixel::setMaxBrightness(0.5);
 
 $font = new Font8x5(24, true, array('Rev!Co Rocks!'));
 
-$phixel = new Phixel(new LPD6803Driver(0, 2000000, 192));
+$driver = new LPD6803Driver();
+$driver->setDevice(0)->setPixelCount(192)->setMode(LPD6803Driver::MODE_SPI);
+
+$phixel = new Phixel($driver);
 $phixel->allOff();
 
 $frame = new FrameBuffer($phixel);
